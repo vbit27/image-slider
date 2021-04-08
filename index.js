c7d1ce63ac90d38326c443e169b981d83cf282f8
Vasil Bit <vasil.bituni@gmail.com>
2021-04-08 08:22:18 +0200

    Add final logic without transition effect

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
+const nextBtn = document.querySelector('.next-btn');
+const backBtn = document.querySelector('.back-btn');
+const slideDots = document.querySelectorAll('.dot');
+
 const swipeImage = (() => {
   const slides = document.querySelectorAll('.single-slide');
 
   let activeImage = 0;
 
   function next() {
-    if (activeImage == slides.length) {
-      activeImage == 0;
-    } else activeImage++;
+    if (activeImage === slides.length - 1) {
+      activeImage = 0;
+    } else {
+      activeImage++;
+    }
     updateImage(activeImage);
+    updateDot(activeImage);
   }
 
   function back() {
     if (activeImage == 0) {
-      activeImage == slides.length;
-    } else activeImage--;
+      activeImage = slides.length - 1;
+    } else {
+      activeImage--;
+    }
     updateImage(activeImage);
+    updateDot(activeImage);
   }
 
+  // find the index of the selected dot
+  function selectDot() {
+    let index = Array.from(slideDots).indexOf(this);
+    activeImage = index;
+    updateImage(index);
+    updateDot(index);
+  }
+
+  //update images and dot style
   function updateImage(index) {
-    hideAll();
+    clearImageClass();
     slides[index].classList.toggle('visible');
   }
 
-  function hideAll() {
+  function updateDot(index) {
+    clearDotClass();
+    slideDots[index].classList.toggle('active');
+  }
+
+  function clearDotClass() {
+    slideDots.forEach((dot) => dot.classList.remove('active'));
+  }
+
+  function clearImageClass() {
     slides.forEach((slide) => slide.classList.remove('visible'));
   }
 
   return {
     next,
     back,
+    selectDot,
   };
 })();
 
-const nextBtn = document.querySelector('.next-btn');
-const backBtn = document.querySelector('.back-btn');
-
 nextBtn.addEventListener('click', swipeImage.next);
+backBtn.addEventListener('click', swipeImage.back);
+slideDots.forEach((dot) => dot.addEventListener('click', swipeImage.selectDot));
